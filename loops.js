ad8c65215d8ea09621f830915c2c7a331c1666a0
Verity Hill <verity.lowenna@gmail.com>
2018-12-30 17:48:54 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -18,6 +18,11 @@ function whileLoop(n) {
 }
 
 function doWhileLoop(array) {
+  var i = 0;
+ 
+  function incrementVariable() {
+    i = i + 1;
+  }
   do {
     incrementVariable()
   } while (array.length > 0 && incrementVariable());

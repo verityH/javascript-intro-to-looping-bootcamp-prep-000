8bcac3061b98933c35519bebbe451c2278fa4f6b
Verity Hill <verity.lowenna@gmail.com>
2018-12-30 17:38:24 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,10 +2,9 @@ function forLoop(array) {
   for( let i = 0; i < 25; i++ ) {
     if( i === 1 ) {
       array.push(`I am 1 strange loop.`);      
-    }else{
+    } else {
       array.push(`I am ${i} strange loops.`);
     }
   }
-
   return array;
 }
